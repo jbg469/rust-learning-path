db65ceb3cf6a2074007ba3450b7643d0139393a1
jbg469 <jbg469@nyu.edu>
2022-09-23 16:46:00 -0400

    hello

---
+++ hello-cargo/target/debug/deps/hello_cargo-26146ca0671eaf3e.d
@@ -0,0 +1,5 @@
+/Users/johnbg/VSCode/rust learning path/hello-cargo/target/debug/deps/hello_cargo-26146ca0671eaf3e: src/main.rs
+
+/Users/johnbg/VSCode/rust learning path/hello-cargo/target/debug/deps/hello_cargo-26146ca0671eaf3e.d: src/main.rs
+
+src/main.rs:
---
+++ hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/Users/johnbg/VSCode/rust\ learning\ path/hello-cargo/target/debug/hello-cargo: /Users/johnbg/VSCode/rust\ learning\ path/hello-cargo/src/main.rs

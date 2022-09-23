377ad4dcb235d6b2862b8c0ce5bb35a7e38ea402
jbg469 <jbg469@nyu.edu>
2022-09-23 17:02:59 -0400

    commit

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -11,6 +11,42 @@ struct MouseClick { x: i64, y: i64 }
 #[derive(Debug)]
 enum WebEvent { WELoad(bool), WEClick(MouseClick), WEKeys(KeyPress) }
 
+// Declare Car struct to describe vehicle with four named fields
+struct Car {
+    color: String,
+    transmission: Transmission,
+    convertible: bool,
+    mileage: u32,
+}
+
+#[derive(PartialEq, Debug)]
+// Declare enum for Car transmission type
+// Corrected code: Enum definition uses commas to separate values
+enum Transmission {
+    Manual,
+    SemiAuto,
+    Automatic,
+}
+
+// Build a new "Car" using the values of three input arguments
+// - Color of the car (String)
+// - Transmission type (enum)
+// - Convertible (boolean, true if the car is a convertible)
+// Return an instance of a "Car" struct with the arrow `->` syntax
+fn car_factory(color: String, transmission: Transmission, convertible: bool) -> Car {
+
+    // Create a new "Car" instance with requested characteristics
+    // - Corrected code: return a "Car" struct
+    // - Bind first three fields to value of corresponding input argument
+    // - Set mileage to 0
+    Car {
+        color: color,
+        transmission: transmission,
+        convertible: convertible,
+        mileage: 0
+    }
+}
+
 // Instantiate a MouseClick struct and bind the coordinate values
 fn main () {
     let click = MouseClick { x: 100, y: 250 };
@@ -31,4 +67,13 @@ fn main () {
     // Print the values in the WebEvent enum variants
     // Use the {:#?} syntax to display the enum structure and data in a readable form
     println!("\nWebEvent enum structure: \n\n {:#?} \n\n {:#?} \n\n {:#?}", we_load, we_click, we_key);
+    // Order three cars
+    let mut car = car_factory(String::from("Red"), Transmission::Manual, false);
+    println!("Car 1 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+    
+    car = car_factory(String::from("Silver"), Transmission::Automatic, true);
+    println!("Car 2 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);    
+    
+    car = car_factory(String::from("Yellow"), Transmission::SemiAuto, false);
+    println!("Car 3 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage); 
 }
